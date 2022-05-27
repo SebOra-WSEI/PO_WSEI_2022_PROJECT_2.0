222f1b3f2e50ae3e51f9f514cce0b9edcb5216ef
SebOra-WSEI <101014313+SebOra-WSEI@users.noreply.github.com>
2022-05-27 18:08:50 +0200

    Add functionality to add new book

---
+++ BOOKSTORE_PROJECT_PO/Dal_Models/AuthorModels/AuthorDalModelForSelector.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BOOKSTORE_PROJECT_PO.Dal_Models.AuthorModels
+{
+    internal class AuthorDalModelForSelector
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BOOKSTORE_PROJECT_PO/Dals/BooksDal.cs
@@ -0,0 +1,26 @@
+﻿using BOOKSTORE_PROJECT_PO.Dal_Models;
+using BOOKSTORE_PROJECT_PO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOKSTORE_PROJECT_PO.Dals
+{
+    internal class BooksDal
+    {
+        private BookstoreDBEntities db = new BookstoreDBEntities();
+
+        public List<BookDalModel> getBooksList =>
+            db.Books.Select(
+                book => new BookDalModel
+                {
+                    Title = book.Title,
+                    PublishedYear = book.PublishedYear,
+                    Author = book.AuthorId,
+                    Status = book.StatusId,
+                    LastCustomer = book.CustomerId
+                }).ToList();
+    }
+}
