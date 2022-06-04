40ce18b4ed449c8eab558e5a8e7aa7b49802bc5a
SebOra-WSEI <101014313+SebOra-WSEI@users.noreply.github.com>
2022-06-04 14:16:55 +0200

    New tests for email

--- BOOKSTORE_PROJECT_PO/Validators/CommonValidator.cs
+++ BOOKSTORE_PROJECT_PO/Validators/CommonValidator.cs
@@ -8,17 +8,16 @@
     public class CommonValidator
     { 
     
-        protected ValidateResult ValidateInput(string text, string fieldName, int maxTextLenght)
+        protected ValidateResult ValidateInput(string text, string fieldName, int maxTextLenght, int minTextLenght)
         {
             if (text.Trim().Length == 0)
-            {
                 return new ValidateResult { ErrorMessage = $"{fieldName} cannot be empty", IsCorrect = false };
-            }
 
             if (text.Trim().Length > maxTextLenght)
-            {
                 return new ValidateResult { ErrorMessage = $"{fieldName} cannot be longer then {maxTextLenght} chars", IsCorrect = false };
-            }
+
+            if (text.Trim().Length < minTextLenght)
+                return new ValidateResult { ErrorMessage = $"{fieldName} too short. Minimal lenght is equal {minTextLenght}", IsCorrect = false};
 
             return new ValidateResult { ErrorMessage = "", IsCorrect = true  };
         }
